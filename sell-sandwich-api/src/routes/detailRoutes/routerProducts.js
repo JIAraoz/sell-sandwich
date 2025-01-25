0a9ae42653b85d8e70849db5c34512c32497ee24
JIAraoz <ignacioaraoz18@gmai.com>
2025-01-24 21:47:10 -0300

    geat-getProducts

--- sell-sandwich-api/src/routes/detailRoutes/routerProducts.js
+++ sell-sandwich-api/src/routes/detailRoutes/routerProducts.js
@@ -1,10 +1,11 @@
 const routerProducts = require("express").Router()
 const createProducts = require('../../controllers/controllerProducts/createProduct')
+const getProducts = require("../../controllers/controllerProducts/getProducts")
 const {validateCreateProduct} = require('../../validations/createProduct')
 
 
 routerProducts.post("/createProduct",validateCreateProduct,createProducts)
-
+routerProducts.get("/getProducts",getProducts)
 
 
 
