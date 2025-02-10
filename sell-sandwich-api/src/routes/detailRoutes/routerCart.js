105449943285f671a7bac280d7cb4ba5a9e5a68f
JIAraoz <ignacioaraoz18@gmai.com>
2025-02-10 14:10:49 -0300

    feat-IsActiveUpdate

--- sell-sandwich-api/src/routes/detailRoutes/routerCart.js
+++ sell-sandwich-api/src/routes/detailRoutes/routerCart.js
@@ -1,9 +1,11 @@
 const { addProductCart } = require('../../controllers/controllerCart/addProductCart')
 const createCart = require('../../controllers/controllerCart/createCart')
 const { getCartById } = require('../../controllers/controllerCart/getCartById')
+const { updateIsActive } = require('../../controllers/controllerCart/updateIsActive')
 const { validateAddCartProduct } = require('../../validations/addCartProduct')
 const { validateCreateCart } = require('../../validations/createCart')
 const { validateGetCartById } = require('../../validations/getCartById')
+const { validateUpdateIsActive } = require('../../validations/updateIsActive')
 
 const routerCart = require('express').Router()
 
@@ -11,5 +13,6 @@ const routerCart = require('express').Router()
 routerCart.post('/createCart',validateCreateCart,createCart)
 routerCart.post('/addCartProduct',validateAddCartProduct,addProductCart)
 routerCart.get('/getCartById/:idCart',validateGetCartById,getCartById)
+routerCart.put('/updateIsActive/:idCart',validateUpdateIsActive,updateIsActive)
 
 module.exports = {routerCart}
