c91e9fe7a87933229a59f86059214e6737387847
JIAraoz <ignacioaraoz18@gmai.com>
2025-02-13 07:43:50 -0300

    feat-updateProduct

--- sell-sandwich-api/src/routes/detailRoutes/routerProducts.js
+++ sell-sandwich-api/src/routes/detailRoutes/routerProducts.js
@@ -3,15 +3,18 @@ const createProducts = require('../../controllers/controllerProducts/createProdu
 const { getProductById } = require("../../controllers/controllerProducts/getProductById")
 const getProducts = require("../../controllers/controllerProducts/getProducts")
 const {updateIsActiveProduct} = require("../../controllers/controllerProducts/updateIsActiveProduct")
+const { updateProduct } = require("../../controllers/controllerProducts/updateProduct")
 const {validateCreateProduct} = require('../../validations/Product/createProduct')
 const { validateGetProductById } = require("../../validations/Product/getProductById")
 const { validateUpdateIsActiveProduct } = require("../../validations/Product/updateIsActiveProduct")
+const { validateUpdateProduct } = require("../../validations/Product/updateProduct")
 
 
 routerProducts.post("/createProduct",validateCreateProduct,createProducts)
 routerProducts.get("/getProducts",getProducts)
 routerProducts.get("/getProductByID/:idProduct",validateGetProductById,getProductById)
 routerProducts.put("/updateIsActiveProduct/:idProduct",validateUpdateIsActiveProduct,updateIsActiveProduct)
+routerProducts.put("/updateProduct/:idProduct",validateUpdateProduct,updateProduct)
 
 
 
