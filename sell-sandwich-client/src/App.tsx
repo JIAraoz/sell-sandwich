f376b7df0819bd7d498838e8c9e97655bf4e9a2f
JIAraoz <ignacioaraoz18@gmai.com>
2025-02-22 19:47:07 -0300

    homepage1

--- sell-sandwich-client/src/App.tsx
+++ sell-sandwich-client/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-
+import HomePage from './components/HomePage'
 function App() {
 
   return (
-    <div className='bg-sky-500 h-screen w-screen flex justify-center items-center'>
-      <h1>
-     PWA test
-      </h1>
+    <div className='bg-slate-400 h-screen w-screen flex justify-center items-center'>
+      <HomePage></HomePage>
     </div>
   )
 }
---
+++ sell-sandwich-client/src/components/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div>
+            <h1>La Buena Onda</h1>
+
+        </div>
+    )    
+}
