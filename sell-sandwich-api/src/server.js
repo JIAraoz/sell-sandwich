2172ad08d500825c907ac98f9eeab7cc4e45793d
JIAraoz <ignacioaraoz18@gmai.com>
2025-02-28 09:40:41 -0300

    static-uploads

--- sell-sandwich-api/src/server.js
+++ sell-sandwich-api/src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {routes} = require('./routes/index')
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json())
 app.use("/", routes)
 module.exports = {app}
