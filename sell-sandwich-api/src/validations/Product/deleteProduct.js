d100b82576709e87f638c191ea36ef4eae55eca2
JIAraoz <ignacioaraoz18@gmai.com>
2025-03-09 09:28:17 -0300

    deleteProduct

---
+++ sell-sandwich-api/src/validations/Product/deleteProduct.js
@@ -0,0 +1,14 @@
+const {param} = require('express-validator')
+const { validateResult } = require("../../helpers/validateHelper")
+const validateDeleteProduct = [
+    param("idProduct")
+    
+    .exists()
+    .withMessage("the idProduct must be a parameter")
+    .isUUID()
+    .withMessage("the idProduct must be an UUID")
+    
+    , (req, res, next) => validateResult(req, res, next)
+]
+
+module.export = {validateDeleteProduct}
