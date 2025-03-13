118da285f6e76ce447d404e065ee0cce0ddf2bb8
JIAraoz <ignacioaraoz18@gmai.com>
2025-03-13 18:40:10 -0300

    hot-fix

--- sell-sandwich-api/src/controllers/controllerProducts/deleteAllImages.js
+++ sell-sandwich-api/src/controllers/controllerProducts/deleteAllImages.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+/* const fs = require('fs')
 const deleteAllImages = (req , res) => {
 fs.readdir("../../../uploads",(err, images)=>{
     if(!err){
@@ -26,4 +26,29 @@ fs.readdir("../../../uploads",(err, images)=>{
 
 module.exports = {
     deleteAllImages
-}
+} */
+    const fs = require('fs');
+    const path = require('path');
+    
+    const deleteAllImages = (req, res) => {
+        const uploadsPath = path.resolve(__dirname, '../../../uploads'); // Ruta absoluta
+    
+        fs.readdir(uploadsPath, (err, images) => {
+            if (err) {
+                return res.status(500).json({
+                    error: "Internal server error. Please try again later.",
+                    details: err.message,
+                });
+            }
+    
+            images.forEach((image) => {
+                console.log(image);
+            });
+    
+            res.status(200).json({
+                images,
+            });
+        });
+    };
+    
+    module.exports = { deleteAllImages };
