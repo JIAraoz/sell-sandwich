cb44ce3d75f13f6c965661436c9d369115de15d2
JIAraoz <ignacioaraoz18@gmai.com>
2025-04-01 17:24:02 -0300

    feat-addBackButton

--- sell-sandwich-client/src/components/Nav.tsx
+++ sell-sandwich-client/src/components/Nav.tsx
@@ -1,14 +1,21 @@
 import { NavLink } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
+import BackButton from "./BackButton";
 export default function Nav(){
+    const url = useLocation().pathname
     return(
         <div className="nav flex justify-around items-center bg-slate-600 h-14">
             <img src="/icon-192x192.png" alt="logo"  className='h-10 object-contain'/>
         
-            <NavLink to='/form'>
-                
-            <button className="border rounded-3xl bg-slate-900 text-xs h-10 p-3">Crear producto</button>
-            </NavLink>
+            {
+             url !== "/form" ? <>
+                           <NavLink to='/form'>    
+             <button className="text-xs h-10 p-3">Crear producto</button>
+             </NavLink>   
+             </> : <BackButton/>
+
+            }
         </div>
     )
 } 
